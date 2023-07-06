22403bc90d04518c9325ff47240bb4f661cad126
TheBerryBucket <jaredptownsend@gmail.com>
2023-07-06 15:43:04 -0600

    Commit fixing results.csv and changing soccer.py

--- Part 4/soccer.py
+++ Part 4/soccer.py
@@ -1,7 +1,7 @@
 import csv
 from recordtype import recordtype
 
-team = recordtype ('Team', 'name win lose tie')
+team = recordtype ('Team', 'name win lose tie winningchance')
 teamsList = []
 
 
@@ -11,16 +11,19 @@ with open ('results.csv', 'r',encoding='utf-8') as csv_file:
 
     for match in matchList:
         homeTeam = [team for team in teamsList if team.name == match['home_team']]
+        
         if not homeTeam:
-            homeTeam = team (match['home_team'], 0 , 0 , 0 )
+            homeTeam = team (match['home_team'], 0 , 0 , 0, 0)
             teamsList.append (homeTeam)
+        
         else:
             homeTeam = homeTeam[0]
-            
-        awayTeam = [team for team in teamsList if team.name == match['away_team']]
+            awayTeam = [team for team in teamsList if team.name == match['away_team']]
+        
         if not awayTeam:
-            awayTeam = team (match['away_team'], 0 , 0 , 0 )
+            awayTeam = team (match['away_team'], 0 , 0 , 0, 0)
             teamsList.append (awayTeam)
+        
         else:
             awayTeam = awayTeam[0]
 
@@ -29,18 +32,26 @@ with open ('results.csv', 'r',encoding='utf-8') as csv_file:
             awayTeam.tie = awayTeam.tie + 1
 
         if match['home_score'] > match['away_score']:
-            homeTeam.win += 1
+            homeTeam.win +=1
             awayTeam.lose = awayTeam.lose + 1
 
         if match['home_score'] < match['away_score']:
             homeTeam.lose = homeTeam.lose + 1
             awayTeam.win = awayTeam.win + 1
-        
+        #homeTeam.winningchance =  homeTeam.win + homeTeam.tie * 0.5
+        #homeTeam.winningchance = team.winningchance / (homeTeam.win + homeTeam.tie + homeTeam.lose)
+        #awayTeam.winningchance = awayTeam.win + awayTeam.tie * 0.5
+        #awayTeam.winningchance = awayTeam.winningchance / (awayTeam.win + awayTeam.tie + awayTeam.lose)
+#        team.remove ('win')
+#        team.remove ('lose')
+#        team.remove ('tie')
 
 
 
 def SortTeams(team):
   return team.name
 
+
 teamsList.sort(key=SortTeams)
+
 print (teamsList)
