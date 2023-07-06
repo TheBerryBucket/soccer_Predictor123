be62f1884d2ceea1cfcd3b3d254ad48090876b40
TheBerryBucket <jaredptownsend@gmail.com>
2023-07-06 14:47:31 -0600

    Part 4 step 6 done

--- Part 4/soccer.py
+++ Part 4/soccer.py
@@ -1,11 +1,10 @@
 import csv
 from recordtype import recordtype
 
-Team = recordtype('Team', 'name win lose tie')
+team = recordtype ('Team', 'name win lose tie')
 teamsList = []
-print('asdf')
-print(['asdf'])
-print([['asdf'], 'asdf'])
+
+
 
 with open ('results.csv', 'r',encoding='utf-8') as csv_file:
     matchList = csv.DictReader(csv_file)
@@ -13,14 +12,14 @@ with open ('results.csv', 'r',encoding='utf-8') as csv_file:
     for match in matchList:
         homeTeam = [team for team in teamsList if team.name == match['home_team']]
         if not homeTeam:
-            homeTeam = Team (match['home_team'], 0 , 0 , 0 )
+            homeTeam = team (match['home_team'], 0 , 0 , 0 )
             teamsList.append (homeTeam)
         else:
             homeTeam = homeTeam[0]
             
         awayTeam = [team for team in teamsList if team.name == match['away_team']]
         if not awayTeam:
-            awayTeam = Team (match['away_team'], 0 , 0 , 0 )
+            awayTeam = team (match['away_team'], 0 , 0 , 0 )
             teamsList.append (awayTeam)
         else:
             awayTeam = awayTeam[0]
@@ -36,5 +35,12 @@ with open ('results.csv', 'r',encoding='utf-8') as csv_file:
         if match['home_score'] < match['away_score']:
             homeTeam.lose = homeTeam.lose + 1
             awayTeam.win = awayTeam.win + 1
-      
-print(teamsList)
+        
+
+
+
+def SortTeams(team):
+  return team.name
+
+teamsList.sort(key=SortTeams)
+print (teamsList)
